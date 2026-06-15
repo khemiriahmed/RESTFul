395ad0c8fb328d7dfd6efb815710488ca506623d
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-15 09:21:00 +0100

    RESTful

--- src/main/java/com/akh/RESTful/controllers/ProductController.java
+++ src/main/java/com/akh/RESTful/controllers/ProductController.java
@@ -3,7 +3,9 @@ package com.akh.RESTful.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +23,14 @@ public class ProductController {
 	public List<Product>getProduits(){
 		return repo.findAll();
 	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<Product>getProduit(@PathVariable int id){
+		Product product = repo.findById(id).orElse(null);
+        if(product == null) {
+        	return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
+	}
+	
 } 
