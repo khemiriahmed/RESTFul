1a3f097587271f62b3d4aeaba846452380092d51
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-15 10:57:53 +0100

    ProductDto

---
+++ src/main/java/com/akh/RESTful/models/ProductDto.java
@@ -0,0 +1,51 @@
+package com.akh.RESTful.models;
+
+import jakarta.validation.constraints.*;
+
+
+public class ProductDto {
+	
+@NotEmpty(message = "The name required")
+private String name;
+@NotEmpty(message = "The brand required")
+private String brand;
+@NotEmpty(message = "The category required")
+private String category;
+@Min(0)
+private double price;
+@Size(min = 10, message = "the Desciption should be at least 10 characters")
+@Size(max = 10, message = "the Desciption cannot  exceed 100 characters")
+private String description;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getBrand() {
+	return brand;
+}
+public void setBrand(String brand) {
+	this.brand = brand;
+}
+public String getCategory() {
+	return category;
+}
+public void setCategory(String category) {
+	this.category = category;
+}
+public double getPrice() {
+	return price;
+}
+public void setPrice(double price) {
+	this.price = price;
+}
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+
+
+}
