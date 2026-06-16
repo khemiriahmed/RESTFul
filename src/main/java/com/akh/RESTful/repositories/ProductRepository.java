f11b1d9b3a7e898d69978b2e8e2b70560966a669
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-16 21:05:48 +0100

    ProductRepository

--- src/main/java/com/akh/RESTful/repositories/ProductRepository.java
+++ src/main/java/com/akh/RESTful/repositories/ProductRepository.java
@@ -1,7 +1,5 @@
 package com.akh.RESTful.repositories;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.akh.RESTful.models.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
